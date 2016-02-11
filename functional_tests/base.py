33ecbb309c56108129a33e81b6e2e8e1376c7561
Marc Kunze <mkunze@gmail.com>
2016-02-10 19:52:05 -0800

    Revert "use wait_for function for URL checks"
    This reverts commit 596de68bd6646ddc4261cd1d95002a8541a9d0e0.

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,7 +1,6 @@
 import os
 from datetime import datetime
 
-DEFAULT_WAIT = 5
 SCREEN_DUMP_LOCATION = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), 'screendumps'
 )
@@ -71,14 +70,3 @@ class FunctionalTest(StaticLiveServerTestCase):
             windowid=self._windowid,
             timestamp=timestamp
         )
-
-    def wait_for(self, function_with_assertion, timeout=DEFAULT_WAIT):
-            start_time = time.time()
-            while time.time() - start_time < timeout:
-                try:
-                    return function_with_assertion()
-                except (AssertionError, WebDriverException):
-                    time.sleep(0.1)
-            # one more try, which will raise any errors if they are outstanding
-            return function_with_assertion()
-
